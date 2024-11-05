6612b619295ef5c0a2917ed2fa3fc75d492a583e
XaipaniKaif <timedenis58@gmail.com>
2024-11-05 15:50:25 +0300

    v 0.01

---
+++ dist/srcs/index.js
@@ -0,0 +1,9 @@
+import 'dotenv/config';
+import { Client, Events } from "discord.js";
+const client = new Client({
+    intents: ['DirectMessages', 'GuildMembers', 'MessageContent', 'Guilds', 'GuildVoiceStates', 'GuildEmojisAndStickers', 'GuildPresences', 'GuildMessages'],
+});
+client.on(Events.ClientReady, () => {
+    console.log('Ready!');
+});
+client.login(process.env.TOKEN);
